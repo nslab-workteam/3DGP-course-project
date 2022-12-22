14ea30395bc9490da49588b21c87effa5f6bc4bf
wei-coding <edward.yu0830@gmail.com>
2022-12-22 23:57:28 +0800

    Monster update

--- Assets/HorrorEnvironment_Hospital/objects/Fighting/FightingTrigger.cs
+++ Assets/HorrorEnvironment_Hospital/objects/Fighting/FightingTrigger.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class FightingTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     [SerializeField] private Animator[] doors;
+    [SerializeField] private Animator monster;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +25,14 @@ public class FightingTrigger : MonoBehaviour
             foreach(Animator i in doors) {
                 i.SetTrigger("CloseDoor");
             }
+            player.GetComponent<PlayerMovement>().maxWalkSpeed = 3;
+            player.GetComponent<PlayerMovement>().maxSprintSpeed = 5;
+            StartCoroutine(DelayStartFighting());
         }
+    }
+
+    IEnumerator DelayStartFighting() {
+        yield return new WaitForSeconds(5f);
+        monster.SetTrigger("Start");
     }
 }
--- Assets/HorrorEnvironment_Hospital/objects/Fighting/MonsterRunStateBehaviour.cs
+++ Assets/HorrorEnvironment_Hospital/objects/Fighting/MonsterRunStateBehaviour.cs
@@ -7,16 +7,29 @@ public class MonsterRunStateBehaviour : StateMachineBehaviour
 {
     private Transform player;
     private NavMeshAgent agent;
+    float timePassed = 0;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GameObject.Find("Monster").GetComponent<NavMeshAgent>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+        agent.SetDestination(player.position);
+        timePassed += Time.deltaTime;
+        if (timePassed >= Random.Range(1, 5)) {
+            int prob = Random.Range(1, 100);
+            if (prob <= 50) {
+                animator.SetTrigger("Attack");
+            } else {
+                animator.SetTrigger("Scream");
+            }
+            timePassed = 0;
+        }
+
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
